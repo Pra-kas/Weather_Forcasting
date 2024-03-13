5fd4eb712a2b7fdab125fc548bc67e9d3d1c73dc
MuthuvelA <muthuveldiary@gmail.com>
2024-03-13 14:24:49 +0530

    fixed css

--- weather-forecast/src/App.jsx
+++ weather-forecast/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
@@ -27,27 +28,26 @@ function App() {
     const newCity = document.getElementById('locationInput').value;
     setCity(newCity);
   };
+  var temp=0;
+  if(weatherData.main && weatherData.main.temp){
+    temp=weatherData.main.temp-273.15
+
+  }
 
   return (
     <>
       <div className="Container">
         <h1>Weather App</h1>
-        <input
-          type="text"
-          id="locationInput"
-          placeholder="Enter city name"
-        />
-        <button id="searchButton" onClick={handleSearch}>
-          Search
-        </button>
+        <input type="text" id="locationInput" placeholder="Enter city name" />
+        <button id="searchButton" onClick={handleSearch}>Search </button>
         <div className="weather-info">
-          <h2>Weather</h2>
-          {weatherData ? (
-            <>
-              <p>Temperature: {weatherData.main && weatherData.main.temp} K</p>
-              <p>Description: {weatherData.weather && weatherData.weather[0].description}</p>
-            </>
-          ) : (
+              <h2>Weather</h2>
+              {weatherData ? (
+              <>
+                <p>Temperature: {temp.toFixed(2)} C</p>
+                <p>Description: {weatherData.weather && weatherData.weather[0].description}</p>
+              </>
+              ) : (
             <p>Please enter a city name and search.</p>
           )}
         </div>
